5a6b7e3c00c968ea43980920e23ea9db5445bd0e
Reshma PS <reshmaps156@gmail.com>
2024-09-17 15:40:08 +0530

    contact us completed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,51 +1,105 @@
-import React from 'react'
+import React from 'react';
 import TextField from '@mui/material/TextField';
+import fog from '../assets/fog.png';
+import { motion } from 'framer-motion';
+
 function Contact() {
-    return (
-        <div className='row  mt-5' >
-            <div className="col-md-6">
+  return (
+    <div className='row mt-5 d-flex justify-content-center' >
+      <motion.h2 
+        className='display-4 text-center text-secondary mb-5'
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        Contact Us
+      </motion.h2>
 
-            </div>
-            <div className="col-md-6  ">
-                <div className='bg-secondary  p-5'>
-                    <div className='mb-3 '>
-                        <TextField id="standard-basic" label="Your Name" variant="standard" color='warning' focused  InputProps={{
-                        sx: {
-                          color: 'white',
-                          '& .MuiSvgIcon-root': {
-                            color: 'grey',
-                          }
-                        },
-                      }}/>
-                    </div>
-                    <div className='mb-3'>
-                        <TextField id="standard-basic" label="Your Email" variant="standard"  color='warning' focused InputProps={{
-                        sx: {
-                          color: 'white',
-                          '& .MuiSvgIcon-root': {
-                            color: 'grey',
-                          }
-                        },
-                      }}/>
-                    </div>
-                    <div className='mb-3'>
-                        <TextField id="standard-basic" label="Share your thoughts" variant="standard"  color='warning' focused InputProps={{
-                        sx: {
-                          color: 'white',
-                          '& .MuiSvgIcon-root': {
-                            color: 'grey',
-                          }
-                        },
-                      }}/>
-                    </div>
-                    <div className='mb-3'>
-                       <button className='btn btn-warning'>Submit</button>
-                    </div>
-                </div>
-
-            </div>
+      <motion.div
+        className="col-md-4"
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.6 }}
+      >
+        <div style={{ maxWidth: '400px' }} className='p-4'>
+          <div className='mb-5'>
+            <TextField 
+              id="standard-basic" 
+              className='w-100' 
+              placeholder='Name' 
+              variant="standard" 
+              color='warning' 
+              focused  
+              InputProps={{
+                sx: {
+                  color: 'white',
+                  '& .MuiSvgIcon-root': {
+                    color: 'grey',
+                  }
+                },
+              }}
+            />
+          </div>
+          <div className='mb-5'>
+            <TextField 
+              id="standard-basic" 
+              className='w-100'  
+              variant="standard"  
+              color='warning' 
+              focused
+              placeholder='Email'
+              InputProps={{
+                sx: {
+                  color: 'white',
+                  '& .MuiSvgIcon-root': {
+                    color: 'grey',
+                  }
+                },
+              }}
+            />
+          </div>
+          <div className='mb-5'>
+            <TextField 
+              id="standard-basic" 
+              className='w-100'  
+              variant="standard"  
+              color='warning'
+              multiline
+              placeholder='Message'
+              rows={2}
+              focused 
+              InputProps={{
+                sx: {
+                  color: 'white',
+                  '& .MuiSvgIcon-root': {
+                    color: 'grey',
+                  }
+                },
+              }}
+            />
+          </div>
+          <div className='mb-5'>
+            <motion.button 
+              className='btn btn-warning w-100'
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+            >
+              Submit
+            </motion.button>
+          </div>
         </div>
-    )
+      </motion.div>
+
+      <motion.div
+        className="col-md-5"
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.6 }}
+      >
+        <img src={fog} alt="Background fog" className='w-100' />
+      </motion.div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
